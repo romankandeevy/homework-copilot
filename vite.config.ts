a6d317a0cd3248c3a3fee2f7d576cd3b3756d4f2
romankandeevy <roman.kandeevy@gmail.com>
2026-08-24 16:59:16 +0300

    Route custom domain through GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS
+  base: process.env.GITHUB_ACTIONS && !process.env.GITHUB_PAGES_CUSTOM_DOMAIN
     ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'homework-copilot'}/`
     : '/',
   plugins: [react()],
